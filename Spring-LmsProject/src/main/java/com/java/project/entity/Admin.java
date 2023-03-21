793559673bd2aaa4084a1c216d81da5028b57df7
dlth4 <dlth4@192.168.219.113>
2023-03-21 18:20:43 +0900

    20230321 18:20 보수중

--- Spring-LmsProject/src/main/java/com/java/project/entity/Admin.java
+++ Spring-LmsProject/src/main/java/com/java/project/entity/Admin.java
@@ -1,5 +1,29 @@
 package com.java.project.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "admin")
 public class Admin {
 
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "AID")
+    private Integer aid;
+
+    @Column(name = "PWD")
+    private String pwd;
+
+    @Column(name = "PHONE")
+    private Integer phone;
+
+    @Column(name = "EMAIL")
+    private String email;
 }
